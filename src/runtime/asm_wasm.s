1103aa7bf03aeb5d385620478ac7ab6533e09450
Ezra e. k. Cooper <ezra.cooper@ed.ac.uk>
2026-06-02 16:59:20 +0100

    fixup

--- src/runtime/asm_wasm.s
+++ src/runtime/asm_wasm.s
@@ -78,7 +78,6 @@ TEXT runtime·gogo(SB), NOSPLIT, $0-8
 		Set RET3
 	Else
 
-		// ASuspend 0   // tag 0, i.e. "gogo"; simply aborts the scheduler stack, goes to $gogo_handler in asm_wasm.wat.
 		I32Const 7  // ignored, needed by wrapper
 		Call runtime·exit_scheduler(SB)
 		Drop  // ignore what's left by the wrapper.

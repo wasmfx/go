aa4b015abf6fb053a5fb8a0e1167ac1146cbd92a
Ezra e. k. Cooper <ezra.cooper@ed.ac.uk>
2026-07-15 14:36:50 +0100

    gofmt the example, and tweak the main loop behavior a bit.

--- src/itersum.go
+++ src/itersum.go
@@ -1,55 +1,59 @@
 package main
 
-import "strconv"
-import "os"
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
 
 // Generate some numbers. (As it happens, the ones from 0..n.)
 // Send them on the given channel.
 func generator(c chan int32, n int32) {
-  for i := int32(0); i < n; i++ {
-    c <- i
-  }
-  close(c)
+	for i := int32(0); i < n; i++ {
+		c <- i
+	}
+	close(c)
 }
 
 // Sum up the numbers sent on the given channcel.
 func summer(c chan int32) int32 {
-  result := int32(0)
-  for {
-    p, more := <- c
-    if !more {
-      break
-    }
-    result += p
-  }
-  return result
+	result := int32(0)
+	for {
+		p, more := <-c
+		if !more {
+			break
+		}
+		result += p
+	}
+	return result
 }
 
 func runsum(n int32) {
-  c := make(chan int32)
-  go generator(c, n)
-  result := summer(c)
-  fmt.Println("Result: ", result)
+	c := make(chan int32)
+	go generator(c, n)
+	result := summer(c)
+	fmt.Println("Result: ", result)
 }
 
 func main() {
-  print("hi")
-  first, err := strconv.Atoi(os.Args[1])
-  if err != nil { panic("arg not parsed as int32") }
-  last, err := strconv.Atoi(os.Args[2])
-  if err != nil { panic("arg not parsed as int32") }
+	print("hi\n") // Confirms we get running, vs dying in the process startup.
+	first, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic("arg not parsed as int32")
+	}
+	last, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		panic("arg not parsed as int32")
+	}
 
-  for i := int32(first); i <= int32(last); i++ {
-    start := time.Now()
-//    runsum(i * 1000000)
-    runsum(i)
-    fmt.Println(i)
-    fmt.Println("Time: ", time.Since(start));
-  }
+	// Jump in increments of 1000 so we get some interestingly different timing data.
+	for i := int32(first); i <= int32(last); i += 1000 {
+		start := time.Now()
+		runsum(i)
+		fmt.Println(i)
+		fmt.Println("Time: ", time.Since(start))
+	}
 }
 
-
 // ok:wq
-
